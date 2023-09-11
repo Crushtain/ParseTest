c12665b3d77fdb268970e3e9c2ea5af51060c91c
Crushtain <vova.muzylev@mail.ru>
2023-09-11 15:19:59 +0300

    Генерация ссылки по ключевому слову через ввод в консоль

--- iternal/newUrl/newUrl.go
+++ iternal/newUrl/newUrl.go
@@ -9,7 +9,7 @@ func NewURL(key string) string {
 	fmt.Print("Введите ключевое слово: ")
 	_, err := fmt.Fscan(os.Stdin, &key)
 	if err != nil {
-		return "Ключевое слово не должно быть пустым"
+		return "Ошибка"
 	}
 
 	url := fmt.Sprintf("https://www.fabrikant.ru/trades/procedure/search/?query=%s&type=0&org_type=org&currency=0&date_type=date_publication&ensure=all&paid_participation=0&okpd2_embedded=1&okdp_embedded=1&count_on_page=10&order_direction=1&type_hash=1561441166", key)
